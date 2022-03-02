702de51d1abce6a8c4a95206a4bce28599c8b748
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-03-02 19:05:13 +0000

    webserver stripping done

--- builds/webserver/configfiles/docker-entrypoint.sh
+++ builds/webserver/configfiles/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Citation: https://www.nixcraft.com/t/nginx-emerg-mkdir-var-lib-nginx-tmp-client-body-failed-2-no-such-file-or-directory/2916
 mkdir -p /run/php-fpm/
+mkdir -p /var/lib/nginx/tmp/
 /usr/sbin/nginx
 /usr/sbin/php-fpm -F
