359e23eff525cd673143b2611c1155688bfb1794
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-03-05 19:11:58 +0000

    dbserver selinux

--- scripts/cleanup-script.sh
+++ scripts/cleanup-script.sh
@@ -17,9 +17,9 @@ docker rmi u2185920/csvs2022-db_i:latest u2185920/csvs2022-db_i:stripped
 docker rmi u2185920/csvs2022-web_i:latest u2185920/csvs2022-web_i:stripped
 
 # Delete SELinux stuff
-sudo sedmodule -r docker_webserver
-sudo sedmodule -r docker_dbserver
+sudo semodule -r docker_webserver
+sudo semodule -r docker_dbserver
 sudo rm -rf ../builds/webserver/tmp
 sudo rm -rf ../builds/dbserver/tmp
 sudo rm ../builds/webserver/docker_webserver.fc ../builds/webserver/docker_webserver.if ../builds/webserver/docker_webserver.pp
-sudo rm ../builds/dbserver/docker_webserver.fc ../builds/dbserver/docker_webserver.if ../builds/dbserver/docker_webserver.pp
+sudo rm ../builds/dbserver/docker_dbserver.fc ../builds/dbserver/docker_dbserver.if ../builds/dbserver/docker_dbserver.pp
