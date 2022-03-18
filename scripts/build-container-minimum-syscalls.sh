f8109030ac9095b6f6d75c3c4cfaccf87361fd92
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-03-18 21:16:15 +0000

    reduced syscalls

--- scripts/build-container-minimum-syscalls.sh
+++ scripts/build-container-minimum-syscalls.sh
@@ -6,12 +6,54 @@
 # Description: Find minimal syscalls for containers by removing from min syscalls that are already generated
 #              (Citation: Peter Norris, https://moodle.warwick.ac.uk/pluginfile.php/2256652/mod_folder/content/0/seccomp/build-minimal-sycalls.sh?forcedownload=1)
 
-# For webserver
+# For dbserver
+cp temp_assets/dbserver/min-syscalls.json temp_assets/dbserver/temp-min-syscalls.json
+while read s
+do
+    echo "$s being removed from min-syscalls.json"
+    grep -vw "\"${s}\"" temp_assets/dbserver/temp-min-syscalls.json > temp_assets/dbserver/tmp.json
+    docker rm -f u2185920_csvs2022-db_c
+    docker run -d \
+        --net u2185920/csvs2022_n \
+        --ip 198.51.100.179 \
+        --hostname db.cyber22.test \
+        -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" \
+        -e MYSQL_DATABASE="csvs22db" \
+        --cpuset-cpus=0 \
+        --memory="300m" \
+        --memory-swap="1g" \
+        --read-only \
+        --tmpfs /tmp \
+        --tmpfs /var \
+        --tmpfs /var/run/mysqld \
+        --tmpfs /var/lib/mysqld \
+        --tmpfs /run/mysqld \
+        -v DBSERVERDATA_VOL:/var/lib/mysql/:rw \
+        --cap-drop=ALL \
+        --security-opt=no-new-privileges \
+        --security-opt seccomp:temp_assets/dbserver/tmp.json \
+        --name u2185920_csvs2022-db_c u2185920/csvs2022-db_i
+    sleep 5
+    
+    curl -s --max-time 3 -X POST http://localhost/action.php \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        -d 'fullname='"$s"'&feedback=can be removed'
+    
+    if [ $( curl --max-time 5 -s http://localhost/index.php | grep -qw ${s} | wc -l ) == "1" ]
+    then
+        cp temp_assets/dbserver/tmp.json temp_assets/dbserver/temp-min-syscalls.json
+        echo "$s can be removed"
+    else
+        echo $s >> temp_assets/dbserver/reduced_min_syscalls
+    fi
+done < temp_assets/dbserver/min_syscalls
+
+For webserver
 cp temp_assets/webserver/min-syscalls.json temp_assets/webserver/temp-min-syscalls.json
 while read s
 do
     echo "$s being removed from min-syscalls.json"
-    grep -v "\"${s}\"" temp_assets/webserver/temp-min-syscalls.json > temp_assets/webserver/tmp.json
+    grep -vw "\"${s}\"" temp_assets/webserver/temp-min-syscalls.json > temp_assets/webserver/tmp.json
     docker rm -f u2185920_csvs2022-web_c
     docker run -d \
         --net u2185920/csvs2022_n \
@@ -43,6 +85,5 @@ do
     fi
 done < temp_assets/webserver/min_syscalls
 
-rm 
-
-# For dbserver
+rm temp_assets/webserver/tmp.json temp_assets/webserver/temp-min-syscalls.json
+rm temp_assets/dbserver/tmp.json temp_assets/dbserver/temp-min-syscalls.json
