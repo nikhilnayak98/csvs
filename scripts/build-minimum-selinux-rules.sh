234ad2c38b8ccf62a794a0831f71d1453b686ef0
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-03-19 20:24:32 +0000

    selinux reduction done

--- scripts/build-minimum-selinux-rules.sh
+++ scripts/build-minimum-selinux-rules.sh
@@ -5,6 +5,57 @@
 # Email: Nikhil-Ranjan.Nayak@warwick.ac.uk
 # Description: Find minimal selinux for containers by removing from each rule that is already generated
 
+# For dbserver
+cd temp_assets/selinux/dbserver
+while read rule
+do
+    echo "$rule being removed from docker_dbserver.te"
+    grep -vw "$rule" docker_dbserver_file > tmp_docker_dbserver.te
+    docker rm -f u2185920_csvs2022-db_c
+    echo "csc" | sudo -S semodule -r tmp_docker_dbserver
+    echo "csc" | sudo -S make -f /usr/share/selinux/devel/Makefile tmp_docker_dbserver.pp
+    echo "csc" | sudo -S semodule -i tmp_docker_dbserver.pp
+    docker run -d \
+        --net u2185920/csvs2022_n \
+        --ip 198.51.100.179 \
+        --hostname db.cyber22.test \
+        -e MYSQL_ROOT_PASSWORD="CorrectHorseBatteryStaple" \
+        -e MYSQL_DATABASE="csvs22db" \
+        --cpuset-cpus=0 \
+        --memory="300m" \
+        --memory-swap="1g" \
+        --read-only \
+        --tmpfs /tmp \
+        --tmpfs /var/run/mysqld \
+        --tmpfs /var/lib/mysqld \
+        --tmpfs /run/mysqld \
+        -v DBSERVERDATA_VOL:/var/lib/mysql/:rw \
+        --cap-drop=ALL \
+        --security-opt=no-new-privileges \
+        --security-opt label:type:tmp_docker_dbserver_t \
+        --name u2185920_csvs2022-db_c u2185920/csvs2022-db_i:stripped
+
+    sleep 5
+
+    curl -s --max-time 5 -X POST http://localhost/action.php \
+       -H "Content-Type: application/x-www-form-urlencoded" \
+       -d 'fullname='"$rule"'&feedback=can be removed'
+    
+    if [ $( curl --max-time 5 -s http://localhost/index.php | grep -w "$rule" | wc -l ) == "1" ]
+    then
+        cp tmp_docker_dbserver.te docker_dbserver_file
+        echo "$rule can be removed"
+    else
+        echo $rule >> min_rules
+    fi
+    echo "csc" | sudo -S rm -rf tmp
+    echo "csc" | sudo -S rm tmp_docker_dbserver.fc tmp_docker_dbserver.if tmp_docker_dbserver.pp
+done < avc_rules
+
+echo "csc" | sudo -S semodule -r tmp_docker_dbserver
+rm tmp_docker_dbserver.te
+
+
 # For webserver
 cd temp_assets/selinux/webserver
 while read rule
