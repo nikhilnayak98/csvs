f2a00bca7725e3e656cb3eb9f76f7629c46cce97
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-03-19 19:26:00 +0000

    final selinux web

---
+++ scripts/build-minimum-selinux-rules.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Author: Nikhil Ranjan Nayak
+# Web: https://github.com/nikhilnayak98/csvs
+# Email: Nikhil-Ranjan.Nayak@warwick.ac.uk
+# Description: Find minimal selinux for containers by removing from each rule that is already generated
+
+# For webserver
+cd temp_assets/selinux/webserver
+while read rule
+do
+    echo "$rule being removed from docker_webserver.te"
+    grep -vw "$rule" docker_webserver_file > tmp_docker_webserver.te
+    docker rm -f u2185920_csvs2022-web_c
+    echo "csc" | sudo -S semodule -r tmp_docker_webserver
+    echo "csc" | sudo -S make -f /usr/share/selinux/devel/Makefile tmp_docker_webserver.pp
+    echo "csc" | sudo -S semodule -i tmp_docker_webserver.pp
+    docker run -d \
+        --net u2185920/csvs2022_n \
+        --ip 198.51.100.180 \
+        --hostname www.cyber22.test \
+        --add-host db.cyber22.test:198.51.100.179 \
+        -p 80:80 \
+        --cpuset-cpus=0 \
+        --memory="100m" \
+        --memory-swap="300m" \
+        --read-only \
+        --tmpfs /var/log/nginx \
+        --tmpfs /var/log/php-fpm \
+        --tmpfs /var/lib/nginx/tmp \
+        --tmpfs /var/run/php-fpm \
+        --tmpfs /run \
+        --cap-drop=ALL \
+        --cap-add=CHOWN --cap-add=SETGID --cap-add=SETUID --cap-add=NET_BIND_SERVICE \
+        --security-opt=no-new-privileges \
+        --security-opt label:type:tmp_docker_webserver_t \
+        --name u2185920_csvs2022-web_c u2185920/csvs2022-web_i:stripped
+    sleep 5
+    if [ $( curl --max-time 5 -s http://localhost/index.php | grep wm00i | wc -l ) == "1" ]
+    then
+        cp tmp_docker_webserver.te docker_webserver_file
+        echo "$rule can be removed"
+    else
+        echo $rule >> min_rules
+    fi
+    echo "csc" | sudo -S rm -rf tmp
+    echo "csc" | sudo -S rm tmp_docker_webserver.fc tmp_docker_webserver.if tmp_docker_webserver.pp
+done < avc_rules
+
+echo "csc" | sudo -S semodule -r tmp_docker_webserver
+rm tmp_docker_webserver.te
